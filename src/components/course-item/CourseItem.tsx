8617730c9a6cf340beed8495010da57a30e4115c
busrapinar <bsrapnrr@gmail.com>
2024-04-16 15:21:44 +0300

    added course-item component

---
+++ src/components/course-item/CourseItem.tsx
@@ -0,0 +1,36 @@
+import React, { useState } from 'react';
+import './index.css'
+
+interface CourseItemProps {
+    title: string;
+    href: string;
+    videoCount?: string;
+}
+
+const CourseItem: React.FC<CourseItemProps> = ({ title, href, videoCount }) => {
+    const [isChecked, setIsChecked] = useState(false);
+
+    const handleCheckboxChange = () => {
+        setIsChecked(!isChecked);
+    };
+
+    return (
+        <article className="group flex items-start gap-3 px-2 py-4 transition border-b border-b-zinc-200 hover:bg-white">
+            <label className="mt-0.5 flex size-5 shrink-0 cursor-pointer items-center justify-center rounded border border-zinc-300 text-zinc-900 shadow-sm">
+                <input
+                    className="pointer-events-none absolute opacity-0"
+                    type="checkbox"
+                    checked={isChecked}
+                    onChange={handleCheckboxChange}
+                />
+                <span className={`checkbox ${isChecked ? 'checked' : ''}`}></span>
+            </label>
+            <h5 className="grow font-medium">
+                <a href={href}>{title}</a>
+            </h5>
+            <span className="shrink-0 whitespace-nowrap rounded bg-zinc-100 px-2 py-0.5 text-sm text-zinc-500">{videoCount}</span>
+        </article>
+    );
+};
+
+export default CourseItem;
--- src/pages/videos/index.tsx
+++ src/pages/videos/index.tsx
@@ -3,6 +3,7 @@ import Description from "../../components/Description";
 import InformationCard from "../../components/InformationCard";
 import Sponsor from "../../components/Sponsor";
 import Headers from "../../components/Headers";
+import CourseItem from "../../components/course-item/CourseItem";
 
 type Props = {};
 
@@ -15,10 +16,11 @@ const Videos = (props: Props) => {
           olarak takip edebilirsiniz." />
           <div className="mt-20 grid grid-cols-2 gap-4 sm:gap-6">
           <InformationCard title="ABONE" informationPath="" information="31 B"/>
-          <InformationCard title="ABONE" informationPath="" information="31 B"/>
+          <InformationCard title="IZLENME" informationPath="" information="1,3 Mn"/>
           </div>
           <Sponsor />
           <Headers header="Frontend 101"/>
+          <CourseItem title="Sıfırdan CSS Eğitimi" videoCount="15 Videos" href="" />
 
     </div>
   );
