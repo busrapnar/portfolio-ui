0887e0f64bc384646eb549af7964fd22eb111858
busrapinar <bsrapnrr@gmail.com>
2024-04-30 01:26:32 +0300

    language toggle added

--- src/layouts/footer/index.tsx
+++ src/layouts/footer/index.tsx
@@ -2,25 +2,27 @@ import { Link } from "react-router-dom";
 import { FaXTwitter, FaInstagram, FaYoutube, FaGithub } from "react-icons/fa6";
 import { MdOutlineEmail } from "react-icons/md";
 import { useTranslation } from "react-i18next";
-import { Button } from "../../components/ui/button";
 
 const Footer = () => {
   const { i18n } = useTranslation();
+  const currentLanguage = i18n.language;
+
   const clickHandle = async (lang: string) => {
     await i18n.changeLanguage(lang);
   };
+
   return (
-    <footer className="mt-40 pb-5 flex justify-between items-center max-w-screen-xl mx-auto ">
+    <footer className="mt-40 pb-5 flex justify-center items-center max-w-screen-xl mx-auto">
       <div className="flex items-center space-x-3">
         <Link
           to="mailto:birdilingilizce@gmail.com"
-          className="flex items-center rounded-full bg-zinc-200 p-3 text-zinc-600 no-underline transition-colors  hover:bg-blue-600 hover:text-white"
+          className="flex items-center rounded-full bg-zinc-200 p-3 text-zinc-600 no-underline transition-colors hover:bg-blue-600 hover:text-white"
         >
           <MdOutlineEmail className="w-6 h-6" />
         </Link>
         <Link
           to="https://instagram.com/birdilingilizce"
-          className="flex items-center rounded-full bg-zinc-200 p-3 text-zinc-600 no-underline transition-colors hover:bg-blue-600 hover:text-white "
+          className="flex items-center rounded-full bg-zinc-200 p-3 text-zinc-600 no-underline transition-colors hover:bg-blue-600 hover:text-white"
         >
           <FaInstagram className="w-6 h-6" />
         </Link>
@@ -42,23 +44,14 @@ const Footer = () => {
         >
           <FaGithub className="w-6 h-6" />
         </Link>
-      </div>
-      <div className="flex gap-0 items-center">
-        <Button variant={"ghost"} onClick={() => clickHandle("tr")}>
-          <img
-            className="w-12 h-8"
-            src="https://upload.wikimedia.org/wikipedia/commons/thumb/8/8e/Flag_of_the_Ottoman_Empire_%281844%E2%80%931922%29.svg/200px-Flag_of_the_Ottoman_Empire_%281844%E2%80%931922%29.svg.png"
-            alt=""
-          />
-        </Button>
 
-        <Button variant={"ghost"} onClick={() => clickHandle("en")}>
+        <button className="rounded-full" onClick={() => clickHandle(currentLanguage === "tr" ? "en" : "tr")}>
           <img
-            className="w-12 h-8"
-            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRnwN0E11tIN7wL2W3XWdyX-Y1PFvCjyfPSnwdr8sEkbRA3oio6Lft7SZlVxEoQ_vwXUoE&usqp=CAU"
-            alt=""
+            className="w-10 h-10 rounded-full"
+            src={currentLanguage === "tr" ?"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRnwN0E11tIN7wL2W3XWdyX-Y1PFvCjyfPSnwdr8sEkbRA3oio6Lft7SZlVxEoQ_vwXUoE&usqp=CAU": "https://www.olkando.com/wp-content/uploads/2024/02/yuvarlak-turk-bayragi-tc-bayrak.png"}
+            alt="language toggle"
           />
-        </Button>
+        </button>
       </div>
     </footer>
   );
