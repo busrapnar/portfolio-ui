e40bf69d0cf9d1a5dff5b44a8f171fc20e7b59de
busrapinar <bsrapnrr@gmail.com>
2024-05-01 21:58:16 +0300

    product-card css edited

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -14,10 +14,10 @@ const ProductCard= ({ imageUrl, title, tags, description, path } : Props) => {
   return (
     <div className="flex flex-col bg-accent/20">
       <div className="flex md:flex-row rounded-xl shadow-lg p-3 mx-auto border border-border">
-        <div className="w-1/3 h-full rounded-lg grid">
-          <img src={imageUrl} className="rounded-xl w-full h-full" />
+        <div className="md:w-5/12 h-full  rounded-lg ">
+          <img src={imageUrl} className="rounded-[4px] w-full h-full" />
         </div>
-        <div className="w-full md:w-2/3 justify-center flex flex-col space-y-2 p-3">
+        <div className="w-full md:w-7/12 justify-center flex flex-col space-y-2 p-3">
           <h3 className="font-bold md:text-3xl text-xl">{title}</h3>
           <div className='flex gap-2'>
             {tags.map((tag, index) => (
@@ -25,7 +25,7 @@ const ProductCard= ({ imageUrl, title, tags, description, path } : Props) => {
             ))}
           </div>
           <p className="md:text-lg text-gray-500 text-base">{description}</p>
-          <Link to={path} className='border border-indigo-500 hover:bg-indigo-500 rounded-sm px-4 py-2 w-32 text-center'>Görüntüle</Link>
+          <Link to={path} className='border border-indigo-500 rounded-[6px] hover:bg-indigo-500  px-4 py-2 w-32 text-center '>Görüntüle</Link>
         </div>
       </div>
     </div>
