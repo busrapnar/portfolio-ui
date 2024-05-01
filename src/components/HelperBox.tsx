a3151f5bb71afbcce20c9ba1d7615260e5596daa
busrapinar <bsrapnrr@gmail.com>
2024-05-01 21:34:45 +0300

    index.css edited

--- src/components/HelperBox.tsx
+++ src/components/HelperBox.tsx
@@ -11,7 +11,7 @@ const HelperBox = ({ title,iconName: Icon , addClass }: Props) => {
     return (
         <div className={`flex flex-col items-center justify-center py-8 gap-8 hover:bg-accent hover:border-indigo-500/50 border px-24 ${additionalClass}`}>
             <Icon size={48} className="" />
-            <h1 className="text-xl font-bold text-center">{title}</h1>
+            <h1 className="text-xl font-bold text-center text-white">{title}</h1>
             
             
         </div>
